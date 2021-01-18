fc35962fd69cedd143f6ae83b3baad5e460707a5
Ashwin (silipwn) [RISCBuntu] <ashwin0nambiar@gmail.com>
2021-01-18 12:04:01 +0530

    [MicroTESK] : Add MicroTESK Plugin

---
+++ generator_plugins/microtesk_plugin/conftest.py
@@ -0,0 +1,13 @@
+# conftest.py
+
+
+def pytest_addoption(parser):
+    parser.addoption("--configlist", action="store")
+
+    parser.addoption("--seed", action="store")
+
+    parser.addoption("--count", action="store")
+
+    parser.addoption("--outputdir", action="store")
+
+    parser.addoption("--moduledir", action="store")
