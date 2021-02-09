22140004533a307748035ba692b55bb38e051a98
Ashwin (silipwn) <mail@as-hw.in>
2021-02-09 12:25:20 +0530

    [Chromite]: Get DUT plugin ready
    Assuming the person has chromite ready
    - Compiles
    - Objdump
    - Simulates on the core

--- dut_plugins/chromite_plugin/gen_framework.py
+++ dut_plugins/chromite_plugin/gen_framework.py
@@ -40,10 +40,14 @@ def compile_cmd_list(asm_dir, yaml_config, gen_suite):
         spike_bin = chromite_yaml_config['spike']['command']
         # Disass
         objdump_bin = chromite_yaml_config['objdump']['command']
+        objdump_args = chromite_yaml_config['objdump']['args']
         # Elf2hex
         elf2hex_bin = chromite_yaml_config['elf2hex']['command']
+        elf2hex_args = chromite_yaml_config['elf2hex']['args']
         # Sim
         sim_bin = chromite_yaml_config['sim']['command']
+        sim_args = chromite_yaml_config['sim']['args']
+        sim_path = chromite_yaml_config['sim']['path']
 
         # # Get files from the directory
         # asm_files = glob.glob(asm_dir+'*.S')
@@ -57,18 +61,45 @@ def compile_cmd_list(asm_dir, yaml_config, gen_suite):
             makefile.write("\nASM_SRC_DIR := " + asm_dir + asm)
             makefile.write("\nCRT_FILE := " + asm_dir + crt_file)
             makefile.write("\nBIN_DIR := bin")
-            makefile.write("\nBASE_SRC_FILES := $(wildcard $(ASM_SRC_DIR)/*.S) \nSRC_FILES := $(filter-out $(wildcard $(ASM_SRC_DIR)/*template.S),$(BASE_SRC_FILES))\nBIN_FILES := $(patsubst $(ASM_SRC_DIR)/%.S, $(BIN_DIR)/%.riscv, $(SRC_FILES))")
+            makefile.write("\nOBJ_DIR := objdump")
+            makefile.write("\nSIM_DIR := sim")
+            makefile.write("\nBASE_SRC_FILES := $(wildcard $(ASM_SRC_DIR)/*.S) \nSRC_FILES := $(filter-out $(wildcard $(ASM_SRC_DIR)/*template.S),$(BASE_SRC_FILES))\nBIN_FILES := $(patsubst $(ASM_SRC_DIR)/%.S, $(BIN_DIR)/%.riscv, $(SRC_FILES))\nOBJ_FILES := $(patsubst $(ASM_SRC_DIR)/%.S, $(OBJ_DIR)/%.objdump, $(SRC_FILES))\nSIM_FILES := $(patsubst $(ASM_SRC_DIR)/%.S, $(SIM_DIR)/%.log, $(SRC_FILES))")
+            # Add all section
+            makefile.write("\n\nall: build objdump sim")
+            makefile.write("\n\t$(info ===== All steps are now finished ====== )")
             # Main part for compliing
             makefile.write("\n\nbuild: $(BIN_FILES)")
             makefile.write("\n\t$(info ===== Build complete ====== )")
             makefile.write("\n\n$(BIN_DIR)/%.riscv: $(ASM_SRC_DIR)/%.S")
             makefile.write("\n\t$(info ================ Compiling asm to binary ============)")
             makefile.write("\n\t" + gcc_compile_bin + " " + gcc_compile_args + " -I " + asm_dir + include_dir + " -o $@ $< $(CRT_FILE) " + linker_args + " $(<D)/$*.ld")
-            makefile.write("\n\n.PHONY : build")
+            # Create an objdump file
+            makefile.write("\n\nobjdump: $(OBJ_FILES)")
+            makefile.write("\n\t$(info ========= Objdump Completed ============)")
+            makefile.write("\n\t$(info )")
+
+            makefile.write("\n$(OBJ_DIR)/%.objdump: $(BIN_DIR)/%.riscv")
+            makefile.write("\n\t$(info ========== Disassembling binary ===============)")
+            makefile.write("\n\t" + objdump_bin + " " + objdump_args + " " + "$< > $@")
+            # Run on target
+            makefile.write("\n\n.ONESHELL:")
+            makefile.write("\nsim: $(SIM_FILES)")
+            makefile.write("\n$(SIM_DIR)/%.log: $(BIN_DIR)/%.riscv")
+            makefile.write("\n\t$(info ===== Now Running on Chromite =====)")
+            makefile.write("\n\tmkdir -p sim/$<")
+            makefile.write("\n\t$(info ===== Creating code.mem ===== )")
+            makefile.write("\n\t" + elf2hex_bin + " " + str(elf2hex_args[0]) + " " + str(elf2hex_args[1]) + " $< " + str(elf2hex_args[2]) + " > sim/$</code.mem ")
+            makefile.write("\n\tcd sim/$<")
+            makefile.write("\n\t $(info ===== Copying chromite_core and files ===== )")
+            makefile.write("\n\tcp " + sim_path + "boot.mem " + sim_path + "chromite_core .")
+            makefile.write("\n\t$(info ===== Now running chromite core ===== )")
+            makefile.write("\n\t ./" + sim_bin + " " + sim_args)
+            # makefile.write("\n\n.PHONY : build")
 
     if gen_suite == 'microtesk':
         make_file = make_file+'.microtesk'
-        # TODO This implementation is still broken, because of file naming difference in microtesk generation :
+        # TODO This implementation is still broken, because of file naming
+        # difference in microtesk generation :
         with open(make_file, "w") as makefile:
             makefile.write("# Auto generated makefile created by river_core compile for microtesk based ASM files")
             makefile.write("\n# Generated on: {0}\n".format(datetime.datetime.now()))
@@ -84,7 +115,10 @@ def compile_cmd_list(asm_dir, yaml_config, gen_suite):
             makefile.write("\n\t$(info ================ Compiling asm to binary ============)")
             makefile.write("\n\t" + gcc_compile_bin + " " + gcc_compile_args + " -o $@ " + linker_args + " $(<D)/$*.ld")
             makefile.write("\n\n.PHONY : build")
-    run_commands.append('make -f {0}'.format(make_file))
+    # TODO Improve Logging for the following steps
+    run_commands.append('make -f {0} build'.format(make_file))
+    run_commands.append('make -f {0} objdump'.format(make_file))
+    run_commands.append('make -f {0} sim'.format(make_file))
     return run_commands
 
 
@@ -95,7 +129,6 @@ def idfnc(val):
 def pytest_generate_tests(metafunc):
 
     if 'test_input' in metafunc.fixturenames:
-        # TODO Change
         logger.debug('Generating commands from pytest_framework')
         test_list = compile_cmd_list(
                                  # metafunc.config.getoption("output_dir"),
