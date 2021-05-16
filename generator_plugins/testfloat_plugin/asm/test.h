4bd499eb81536bca818b3434db7d62e2d58ce9d9
Neel Gala <neelgala@incoresemi.com>
2021-05-16 11:05:05 +0530

    updated macros for single and 3 register ops

--- generator_plugins/testfloat_plugin/asm/test.h
+++ generator_plugins/testfloat_plugin/asm/test.h
@@ -8,15 +8,6 @@
   #define LREG flw
 #endif
 
-#define TEST_AR_OP(inst, destreg, freg1, freg2, rm, correctval, correctflags, val1, val2) \
-    li x1, val1;  \
-    XTOF freg1, x1;  \
-    li x1, val2;  \
-    XTOF freg2, x1;  \
-    csrrwi x0, frm, rm;   \
-    inst destreg, freg1, freg2;\
-    csrrs x1, fflags, x0;   \
-
 #define TEST_RR_OP(inst, destreg, freg1, freg2, rm, offset1, offset2) \
     csrrwi x0, frm, rm;   \
     LREG freg1, offset1(x1); \
@@ -24,34 +15,17 @@
     inst destreg, freg1, freg2;\
     csrrs x2, fflags, x0;   \
 
-#define TEST_CVT_OP(inst, destreg, reg1, rm, correctval, correctflags, val1) \
-    li x1, val1;  \
-    XTOF reg1, x1;  \
+#define TEST_RRR_OP(inst, destreg, freg1, freg2, freg3, rm, offset1, offset2, offset3) \
     csrrwi x0, frm, rm;   \
-    inst destreg, reg1;   \
-    csrrs x1, fflags, x0;
+    LREG freg1, offset1(x1); \
+    LREG freg2, offset2(x1); \
+    LREG freg3, offset3(x1); \
+    inst destreg, freg1, freg2, freg3;\
+    csrrs x2, fflags, x0;   \
 
-#define TEST_CMP_OP(inst, destreg, freg1, freg2, rm, correctval, correctflags, val1, val2) \
-    li x1, val1;  \
-    XTOF freg1, x1;  \
-    li x1, val2;  \
-    XTOF freg2, x1;  \
+#define TEST_R_OP(inst, destreg, freg1, rm, offset1) \
     csrrwi x0, frm, rm;   \
-    inst destreg, freg1, freg2;   \
-    csrrs x1, fflags, x0;
+    LREG freg1, offset1(x1); \
+    inst destreg, freg1;\
+    csrrs x2, fflags, x0;   \
 
-#define TEST_FMADD_OP(inst, destreg, freg1, freg2, freg3, rm, correctval, correctflags, val1, val2, val3) \
-    li x1, val1;  \
-    XTOF freg1, x1;  \
-    li x1, val2;  \
-    XTOF freg2, x1;  \
-    li x1, val3;  \
-    XTOF freg3, x1;  \
-    csrrwi x0, frm, rm;   \
-    inst destreg, freg1, freg2, freg3;   \
-    li x2, correctval;  \
-    FTOX x1, destreg;  \
-    bne x1, x2, rvtest_code_end; \
-    csrrs x1, fflags, x0;   \
-    li x2, correctflags;  \
-    bne x1, x2, rvtest_code_end;
