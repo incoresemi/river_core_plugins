752f66e7e789116f310f7f9d8027a1fdadbc637e
Ashwin (silipwn) <mail@as-hw.in>
2021-05-28 16:27:39 +0530

    [Plugin]: Make CTG compatible with dev branch
    - Also removes riscof parts

--- generator_plugins/ctg_plugin/ctg_plugin.py
+++ generator_plugins/ctg_plugin/ctg_plugin.py
@@ -15,14 +15,14 @@ import re
 from river_core.log import logger
 import river_core.utils as utils
 from river_core.constants import *
-from riscv_ctg.main import cli
-
-from riscof.utils import shellCommand
 
 gen_hookimpl = pluggy.HookimplMarker("generator")
 
 this = os.path.abspath(os.path.dirname(__file__))
+
+
 class ctg_plugin(object):
+
     @gen_hookimpl
     def pre_gen(self, spec_config, output_dir):
         '''
@@ -44,56 +44,88 @@ class ctg_plugin(object):
         self.jobs = int(spec_config['jobs'])
         self.randomize = bool(spec_config['randomize'])
         self.isa = str(spec_config['isa'])
+        self.config_isa = str(spec_config['test_cfg'])
         self.gen_config = os.path.abspath(spec_config['ctg_gen_config'])
-        self.config_file = os.path.abspath(spec_config['riscof_config'])
+        # self.config_file = os.path.abspath(spec_config['riscof_config'])
 
     @gen_hookimpl
-    def gen(self, gen_config, module_dir, output_dir):
+    def gen(self, module_dir, output_dir):
         output_dir = os.path.abspath(output_dir)
         logger.debug("CTG Gen phase.")
-        asm_path = os.path.join(output_dir,"ctg/asm/")
-        pytest_file = os.path.join(this,'gen_framework.py')
+        asm_path = os.path.join(output_dir, "ctg/asm/")
+        pytest_file = os.path.join(this, 'gen_framework.py')
         os.makedirs(asm_path)
-        # ctg("debug",os.path.join(output_dir,asm_path), self.randomize, self.xlen, self.cgf_files, self.jobs, self.base_isa)
-        # ctg_command = 'riscv_ctg -v info -bi {0} -d {1} -p {2}'.format(self.base_isa, asm_path, self.jobs)+('' if not self.randomize else ' -r ') + '-cf ' + ' -cf '.join(self.cgf_files)
-        # shellCommand(ctg_command).run(cwd=os.path.join(output_dir,"ctg/"))
         report_file_name = '{0}/{1}_{2}'.format(
-            os.path.join(output_dir,".json/"), self.name,
+            os.path.join(output_dir, ".json/"), self.name,
             datetime.datetime.now().strftime("%Y%m%d-%H%M"))
         pytest.main([
-            pytest_file, '-n={0}'.format(self.jobs), '--configfile={0}'.format(self.gen_config), '-v',
-            '--isa={0}'.format(self.isa), '--jobs={0}'.format(self.jobs),
+            pytest_file, '-n={0}'.format(self.jobs),
+            '--configfile={0}'.format(self.gen_config), '-v',
+            '--isa={0}'.format(self.config_isa), '--jobs={0}'.format(self.jobs),
             '--html={0}/reports/ctg.html'.format(output_dir),
             '--report-log={0}.json'.format(report_file_name),
             '--self-contained-html', '--output_dir={0}'.format(asm_path),
-            '--module_dir={0}'.format(this),('' if not self.randomize else '--randomize')
+            '--module_dir={0}'.format(this),
+            ('' if not self.randomize else '--randomize')
         ])
-        work_dir = os.path.join(output_dir,"ctg/riscof_work/")
-        includes = os.path.join(output_dir,"ctg/asm/env/")
-        riscof_command = "riscof testlist --config {0} --suite {1}".format(self.config_file,asm_path)
-        shellCommand(riscof_command).run(cwd=os.path.join(output_dir,"ctg/"))
-        riscof_test_list = utils.load_yaml(os.path.join(work_dir,"test_list.yaml"))
+        # work_dir = os.path.join(output_dir, "ctg/riscof_work/")
+        includes = os.path.join(output_dir, "ctg/asm/env/")
         test_list = {}
-        for entry in riscof_test_list.keys():
-            key = os.path.basename(entry)[:-2]
-            new_entry = {}
-            new_entry["isa"] = riscof_test_list[entry]["isa"]
-            new_entry['work_dir'] = riscof_test_list[entry]['work_dir']
-            new_entry['asm_file'] = riscof_test_list[entry]['test_path']
-            new_entry['coverage_labels'] = riscof_test_list[entry]['coverage_labels']
-            new_entry['generator'] = self.name
-            new_entry['include'] = [includes]
-            new_entry['cc'] = 'riscv64-unknown-elf-gcc'
-            new_entry['result'] = "Unavailable"
-            new_entry['cc_args'] = ' -mcmodel=medany -static -std=gnu99 -O2 -fno-common -fno-builtin-printf -fvisibility=hidden '
-            new_entry['linker_args'] = '-static -nostdlib -nostartfiles -lm -lgcc -T'
-            new_entry['linker_file'] = ''
-            new_entry['mabi'] = ''
-            new_entry['macros'] = riscof_test_list[entry]['macros']
-            new_entry['march'] = new_entry['isa']
-            test_list[key] = new_entry
+        asm_test_list = glob.glob(asm_path + '*.S')
+        # asm_templates = glob.glob(asm_dir+'/**/*.S')
+        for test in asm_test_list:
+            with open(test, 'r') as file:
+                test_asm = file.read()
+            isa = set()
+            isa.add('i')
+            xlen = 64
+            dist_list = re.findall(r'^#\s*(rel_.*?)$', test_asm, re.M | re.S)
+            for dist in dist_list:
+                ext = dist.split(':')[0][4:].split('.')[0]
+                ext_count = int(dist.split(':')[1])
+
+                if ext_count != 0:
+                    if 'rv64' in ext:
+                        xlen = 64
+                    if 'm' in ext:
+                        isa.add('m')
+                    if 'a' in ext:
+                        isa.add('a')
+                    if 'f' in ext:
+                        isa.add('f')
+                    if 'd' in ext:
+                        isa.add('d')
+                    if 'c' in ext:
+                        isa.add('c')
+            canonical_order = {'i': 0, 'm': 1, 'a': 2, 'f': 3, 'd': 4, 'c': 5}
+            canonical_isa = sorted(list(isa), key=lambda d: canonical_order[d])
+
+            march_str = 'rv' + str(xlen) + "".join(canonical_order)
+            if xlen == 64:
+                mabi_str = 'lp64'
+            elif 'd' not in march_str:
+                mabi_str = 'ilp32d'
+
+            base_key = os.path.basename(test)[:-2]
+            test_list[base_key] = {}
+            test_list[base_key]['generator'] = self.name
+            test_list[base_key]['include'] = [includes]
+            test_list[base_key]['work_dir'] = output_dir + '/ctg/asm/'
+            test_list[base_key][
+                'asm_file'] = output_dir + '/ctg/asm/' + base_key + '.S'
+            test_list[base_key]['isa'] = self.isa
+            test_list[base_key]['march'] = march_str
+            test_list[base_key]['mabi'] = mabi_str
+            test_list[base_key]['cc'] = 'riscv64-unknown-elf-gcc'
+            test_list[base_key][
+                'cc_args'] = ' -mcmodel=medany -static -std=gnu99 -O2 -fno-common -fno-builtin-printf -fvisibility=hidden '
+            test_list[base_key]['linker_args'] = ''
+            test_list[base_key]['linker_file'] = ''
+            test_list[base_key]['extra_compile'] = []
+            test_list[base_key]['result'] = 'Unavailable'
+
         return test_list
 
     @gen_hookimpl
-    def post_gen(self, output_dir, regressfile):
+    def post_gen(self, output_dir):
         pass
