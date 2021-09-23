4f9b6c65c1032af5ad05e86404eeb5f7d07ca67c
alenkruth <alenkruth@gmail.com>
2021-09-23 17:42:41 +0530

    update pytest option name

--- generator_plugins/uatg_plugin/conftest.py
+++ generator_plugins/uatg_plugin/conftest.py
@@ -1,10 +1,16 @@
+# See LICENSE.incore for details
+# Author Name: Alenkruth K M
+# Author Email id: alenkruth.km@incoresemi.com
+# Author Company: InCore Semiconductors Pvt. Ltd.
+
+
 # conftest.py
 def pytest_html_report_title(report):
     report.title = "Generation Report - UATG"
 
 
 def pytest_addoption(parser):
-    parser.addoption("--dut_config", action="store")
+    parser.addoption("--config", action="store")
 
     parser.addoption("--modules_dir", action="store")
 
