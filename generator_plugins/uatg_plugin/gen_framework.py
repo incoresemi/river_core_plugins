3be10cc43233f87a3677cbf77e3ffbe53af67bd0
alenkruth <alenkruth@gmail.com>
2021-09-23 17:43:12 +0530

    update for new UATG configure command

--- generator_plugins/uatg_plugin/gen_framework.py
+++ generator_plugins/uatg_plugin/gen_framework.py
@@ -1,4 +1,7 @@
-# See LICENSE for details
+# See LICENSE.incore for details
+# Author Name: Alenkruth K M
+# Author Email id: alenkruth.km@incoresemi.com
+# Author Company: InCore Semiconductors Pvt. Ltd.
 
 import os
 import sys
@@ -13,26 +16,26 @@ import datetime
 import pytest
 
 
-def gen_cmd_list(dut_config_file, work_dir, linker_dir, module, output_dir,
-                 module_dir, gen_cvg, modules_dir, alias_file):
+def gen_cmd_list(config, work_dir, linker_dir, module, output_dir, module_dir,
+                 gen_cvg, modules_dir, alias_file):
 
     logger.debug('Generating commands for gen plugin')
     run_command = []
     run_command.append(
-        "uatg generate --verbose debug --dut_config {0} --module_dir {1} --work_dir {2} --modules {3} --linker_dir {4} --alias_file {5} {6}"
-        .format(dut_config_file, modules_dir, work_dir, module, linker_dir,
-                alias_file, gen_cvg))
+        "uatg generate --verbose debug --configuration {0} --configuration {1} --configuration {2} --configuration {3} --module_dir {4} --work_dir {5} --modules {6} --linker_dir {7} --alias_file {8} {9}"
+        .format(config[0], config[1], config[2], config[3], modules_dir,
+                work_dir, module, linker_dir, alias_file, gen_cvg))
     logger.debug(run_command)
     return run_command
 
 
 def idfnc(val):
-    return 'Generating Test-list using utg'
+    return 'Generating Test-list using UATG'
 
 
 def pytest_generate_tests(metafunc):
     if 'test_input' in metafunc.fixturenames:
-        test_list = gen_cmd_list(metafunc.config.getoption("dut_config"),
+        test_list = gen_cmd_list(metafunc.config.getoption("config"),
                                  metafunc.config.getoption("work_dir"),
                                  metafunc.config.getoption("linker_dir"),
                                  metafunc.config.getoption("module"),
